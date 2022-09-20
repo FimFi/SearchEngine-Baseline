a830c9608d46ce800bea417a52fb0c7b0b32f95c
FimFi <55874743+FimFi@users.noreply.github.com>
2022-09-20 13:20:48 +0200

    yessir

---
+++ SearchAPI/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        Database mDatabase;
+
+        Dictionary<string, int> mWords;
+
+        public SearchController(Database database)
+        {
+            mDatabase = database;
+            mWords = mDatabase.GetAllWords();
+        }
+
+        [HttpGet("{id}")]
+        public int GetIdOf(string word)
+        {
+            if (mWords.ContainsKey(word))
+                return mWords[word];
+            return -1;
+        }
+        [Route("[controller]/GetDocuments")]
+        [HttpGet]
+        public List<KeyValuePair<int, int>> GetDocuments([FromQuery]List<int> wordIds)
+        {
+            return mDatabase.GetDocuments(wordIds);
+        }
+        [HttpGet("getDocumentDetails")]
+        public List<string> GetDocumentDetails([FromQuery] List<int> docIds)
+        {
+            return mDatabase.GetDocDetails(docIds);
+        }
+    }
+}
